aae00207b2ff2ec27c3e3019e5b45b824cd3dd22
Saturn12132 <99106795+Saturn12132@users.noreply.github.com>
2022-08-10 21:33:53 -0700

    Update ewfjhgidfshwiefdhreidfhwqvpidberdvhrioegufigegwsigwiosdgiuwegierjwglisigoiwsaiggusydiugrysduz.lua

--- ewfjhgidfshwiefdhreidfhwqvpidberdvhrioegufigegwsigwiosdgiuwegierjwglisigoiwsaiggusydiugrysduz.lua
+++ ewfjhgidfshwiefdhreidfhwqvpidberdvhrioegufigegwsigwiosdgiuwegierjwglisigoiwsaiggusydiugrysduz.lua
@@ -1,3 +1,24 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Saturn12132/MaterialLua/master/Module.lua"), "Material Lua")()
+
+local Lib = Library:Load({
+    Title = "Utmm kit GUI",
+    SizeX = 300,
+    SizeY = 200,
+    Position = "Top-Right",
+    XAlignment = "Center",
+    Style = 2,
+    Theme = "Jester"
+})
+
+local Main = Lib:New({
+    Title = "Key System"
+})
+
+Main:TextField({
+    Text = "Insert Key here",
+    Callback = function(KEY)
+getgenv().Key = ""..KEY..""
+if getgenv().Key == "32bug84hjiwqofg328b28g2hewoig2hgb2u9e28gh9gbug824iqv8oefu12eh1919fhqg81ihurqoi19gh283hfeg8h2ejif4hueqdwr32g9ybfnuwdji13g2unef24gud3nfbgfijoenbf2gyuifenbuvybuingrbnfeimg" then
 game["Run Service"].RenderStepped:connect(
    function()
        setscriptable(game.Players.LocalPlayer, "SimulationRadius", true)
@@ -19,11 +40,12 @@ end
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Saturn12132/MaterialLua/master/Module.lua"), "Material Lua")()
 
 local Lib = Library:Load({
-    Title = "Utmm kit GUI - Made by Nekai#0289 | I was at something important",
+    Title = "Utmm kit GUI",
     SizeX = 500,
     SizeY = 360,
     Position = "Top-Right",
     XAlignment = "Center",
+    Style = 2,
     Theme = "Jester"
 })
 
@@ -96,4 +118,7 @@ Main:GuiSettings({
     Theme = false,
     Options = false,
     Rejoin = false,
+})
+end
+    end,
 })
